5c502758d57f3ea2e01d2ba7d93dff2c97ae6c4d
Yayra de Souza <yayradesouza1@gmail.com>
2023-08-24 09:14:48 +0000

    new file:   App.css 	new file:   App.js 	new file:   App.test.js 	new file:   index.css 	new file:   index.js 	new file:   logo.svg 	new file:   reportWebVitals.js 	new file:   setupTests.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p> I am Yayra de Souza, this is my first React application </p>
+    </div>
+  );
+}
+
+export default App;
